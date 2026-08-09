74fb191b46dc10f1dfd47d20748ec536913776f4
Konitsuga <25f2008715@ds.study.iitm.ac.in>
2026-08-09 12:56:31 +0530

    final database schema setup

--- models.py
+++ models.py
@@ -14,12 +14,13 @@ class User(db.Model,UserMixin):
     role=db.Column(db.Integer,nullable=False) #0/1/2
     trekkers=db.relationship("Tr_Profile",cascade="all,delete",backref='users')
     tStaffs=db.relationship("Ts_Profile",cascade="all,delete",backref='users')
+    bookings=db.relationship('Booking', cascade='all, delete', backref='users')
 
 
 class Tr_Profile(db.Model):
     __tablename__='tr_profile'
     id=db.Column(db.Integer,primary_key=True)
-    trID=db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+    trID=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
     full_name=db.Column(db.String,nullable=False)
     address=db.Column(db.String,nullable=False)
     phno=db.Column(db.String,nullable=False)
@@ -29,35 +30,40 @@ class Tr_Profile(db.Model):
 class Ts_Profile(db.Model):
     __tablename__='ts_profile'
     id=db.Column(db.Integer,primary_key=True)
-    tsID=db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+    tsID=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
     full_name=db.Column(db.String,nullable=False)
     phno=db.Column(db.String,nullable=False)
     address=db.Column(db.String,nullable=False)
-    status=db.Column(db.Integer,nullable=False,default=0) #0-register, 1-approved, 2-deactivated, 3-open, 4-full
+    status=db.Column(db.Integer,nullable=False,default=0)
+    treks = db.relationship('Trek', backref='staff')
+    #0-register, 1-approved, 2-deactivated, 3-open, 4-full
 
 
-class booking(db.Model):
+class Booking(db.Model):
     __tablename__='booking'
     id=db.Column(db.Integer,primary_key=True)
-    ts_id=db.Column(db.Integer,db.ForeignKey('ts_profile.id'),nullable=False)
+    tk_id=db.Column(db.Integer, db.ForeignKey('trek.id'), nullable=False)
     tr_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
-    token_no=db.Column(db.Integer,nullable=False)
-    booking_time=db.Column(db.String,nullable=False)
-    status=db.Column(db.String,default='b') #b-booked,d-completed,c-cancelled
-
-    treks=db.relationship("Trek",cascade="all,delete",backref='bookings') 
+    booking_time=db.Column(db.DateTime) # FOR LATER
+    status=db.Column(db.String,default='Booked') #booked/cancelled/completed
 
 
 class Trek(db.Model):
     __tablename__='trek'
     id=db.Column(db.Integer,primary_key=True)
-    bk_id=db.Column(db.Integer,db.ForeignKey('booking.id'),nullable=False)
+    ts_id=db.Column(db.Integer,db.ForeignKey('ts_profile.id'),nullable=False)
+
     name = db.Column(db.String, nullable=False)
-    difficulty = db.Column(db.Integer, nullable=False, default=0)
-    dt_time=db.Column(db.String,nullable=False)
-    status = db.Column(db.String, nullable=False, default='0') # 0/1/2/3/4
+    location=db.Column(db.String, nullable=False)
+    duration=db.Column(db.Integer, nullable=False)
+    difficulty = db.Column(db.String, nullable=False, default='Moderate')
+
+    start_dt = db.Column(db.Date, nullable=False)
+    end_dt=db.Column(db.Date,nullable=False)
+    status = db.Column(db.String, nullable=False, default='Pending')
     slots = db.Column(db.Integer, nullable=False)
     details=db.Column(db.String,nullable=True)
+    bookings=db.relationship('Booking', cascade='all, delete', backref='treks')
 
 # User (role: Admin / Trek Staff / Trekker)
 # Trek (Trek Name, Difficulty, Duration, Available Slots, Assigned Staff, Status, etc)
